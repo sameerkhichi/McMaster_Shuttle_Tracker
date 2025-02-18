d8e8ceac9170479bd586557c4014156bf43a62d5
Sameer <sameerrkhichi@gmail.com>
2025-02-18 01:20:13 -0500

    adding API routes

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
-from flask import Flask, request, jsonify, redirect, url_for
+from flask import Flask
+from routes import app_routes
 from models import db
 
 #initializing flask and the database
 app = Flask(__name__)
+app.register_blueprint(app_routes)
 app.config.from_object('config')
 db.init_app(app)
 
 #create the tables in the database
 with app.app_context():
     db.create_all()
+
+#for debugging
+##if __name__ == '__main__':
+##    app.run(debug=True)
---
+++ routes.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, Blueprint, jsonify, redirect, url_for, render_template
+from models import db, BusLocation
+
+#blueprints for the routes
+app_routes = Blueprint('app_routes', __name__)
+
+@app_routes.route('/bus-locations', methods=['GET'])
+def get_bus_locations():
+
+    #get all the bus location records from the database
+    locations = BusLocation.query.all()
+    bus_data = []
+
+    for loc in locations:
+        bus_data.append({
+            "bus_id": loc.bus_id,
+            "lat": loc.latitude,
+            "lon": loc.longitude,
+            "timestamp": loc.timestamp
+        })
+    
+    return jsonify(bus_data), 200
+
+#base url root
+@app_routes.route('/')
+def web_interface():
+
+    locations = BusLocation.query.all()
+
+    return render_template('placeholder', locations) #replace placeholder with the JS file for frontend
+
