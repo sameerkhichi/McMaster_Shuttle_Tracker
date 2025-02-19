6fb3141a77c4dcf2fc6120ac4b1a52da462d6ade
Sameer <sameerrkhichi@gmail.com>
2025-02-18 20:34:29 -0500

    adding a frontend page to test routes.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("/bus-locations")
+      .then((response) => response.json())
+      .then((data) => setData(data))
+      .catch((error) => console.error("Error fetching data:", error));  
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Parking Services Shuttle Tracker</h1>
+      <h2>Bus Locations</h2>
+      <ul>
+        {data.map((bus, index) =>(
+          <li key={index}>
+            Bus {bus.bus_id}: {bus.lat}, {bus.lon} at {bus.timestamp}
+          </li>
+        ))}  
+      </ul>
     </div>
   );
 }
