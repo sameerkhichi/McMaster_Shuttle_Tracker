539d5dd040ae82ff022559bc89be62e2589b47ba
Sameer Khichi <sameerrkhichi@gmail.com>
2025-07-27 20:19:37 -0400

    Updating sqlalchemy engine options to prevent app from exceeding connection limits.

--- server/configuration/config.py
+++ server/configuration/config.py
@@ -1,9 +1,13 @@
-#keep this private - database/Mysql configuration
+#database/MySQL configuration
 import os
 
 SQLALCHEMY_DATABASE_URI = os.environ.get('MYSQL_URI')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 SQLALCHEMY_ENGINE_OPTIONS = {
-    'pool_pre_ping': True, #preping to ensure active status
-    'pool_recycle': 14400 #recycle connecting every 4 hours avoiding sql timing out
+    'pool_pre_ping': True,       #Checks if connection is alive before using
+    'pool_recycle': 280,         #Recycle idle connections every ~5 minutes
+    'pool_size': 5,              #Max 5 persistent connections (free-tier limit)
+    'max_overflow': 0,           #Don't allow extra (temporary) connections
+    'pool_timeout': 10           #Wait max 10 seconds before giving up on getting a connection
 }
